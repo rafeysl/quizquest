0ff778bf193e7cd2b96950c6e1e5adec92f37b6b
rafeysl <rfeyfaasl@gmail.com>
2026-05-06 14:36:44 +0700

    feat: install breeze auth

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Http\Controllers\QuizController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/quizzes', [QuizController::class, 'index']);
-Route::get('/quiz', function () {
-    return view('quiz_view');
+Route::get('/', function () {
+    return view('welcome');
 });
-Route::get('/admin/dashboard', function () {
-    return view('admin_dashboard');
-})->name('admin.dashboard');
 
-Route::get('/student/quiz', function () {
-    return view('student_quiz');
-})->name('student.quiz');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
