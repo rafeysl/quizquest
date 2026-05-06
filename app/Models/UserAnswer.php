90f373878b5dae90aac49c634e25dc86793a7f06
rafeysl <rfeyfaasl@gmail.com>
2026-05-06 17:21:25 +0700

    tes push ulang

--- app/Models/UserAnswer.php
+++ app/Models/UserAnswer.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class UserAnswer extends Model
+class UserAnswers extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'user_answers';
+
+    protected $fillable = [
+        'attempt_id', 'question_id',
+        'option_id', 'essay_answer',
+        'is_correct', 'points_earned'
+    ];
+
+    protected $casts = [
+        'is_correct' => 'boolean'
+    ];
+
+    public function attempt()
+    {
+        return $this->belongsTo(QuizAttempt::class, 'attempt_id');
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function option()
+    {
+        return $this->belongsTo(Option::class);
+    }
 }
